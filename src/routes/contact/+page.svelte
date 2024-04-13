bf14c4434f1af61f1e9e1e70916d8535e3ad2a93
Sakii1231e <ksakshat7@gmail.com>
2024-04-14 01:37:53 +0545

    images prod load

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { PUBLIC_PROD } from '$env/static/public';
+
+    console.log(PUBLIC_PROD)
+    const isTrue = true;
+
+    if (eval(PUBLIC_PROD)) {
+        console.log("The value is true.");
+    } else {
+        console.log("The value is false.");
+    }
+
+
+</script>
+
+
+
